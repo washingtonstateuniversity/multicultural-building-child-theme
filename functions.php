a0eb1b4ca5f8d38e86de1c3e13a6b47d9c2a65b6
Nate Owen <nateowen@ucghosts-MacBook-Pro.local>
2016-02-26 16:56:07 -0800

    fixes

--- functions.php
+++ functions.php
@@ -44,8 +44,8 @@ function gform_dequeue_script_list() {
     } 
 }
 
-add_filter('wp_kses_allowed_html', 'allow_factor_attribute', 10);
+add_filter('wp_kses_allowed_html', 'allow_factor_attribute', 1);
 public function allow_factor_attributes( $tags ){
     $tags['div']['data-factor']= true;
-    
+    return $tags;   
 }
